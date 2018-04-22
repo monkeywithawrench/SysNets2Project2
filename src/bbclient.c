8b980728b4996ce3bfadc41bcfc3367e38e5db94
Jeff <monkeybuilderman@gmail.com>
2018-04-22 17:24:26 -0500

    Client now receives server response

--- src/bbclient.c
+++ src/bbclient.c
@@ -23,17 +23,15 @@
 //The client
 int main(int argc, char *argv[]){
 
-	struct sockaddr_in serverAddr, clientAddr;//IPV4
-	struct hostent *server;//store info of host
+	struct sockaddr_in serverAddr, clientAddr;//IPV4 //TODO these probably aren't needed anymore
+	struct hostent *host;//store info of host  //TODO these probably aren't needed anymore
 	int sockfd, clientPort, serverPort, n;
-	char buffer[BUFFER_SIZE]; //TODO handle this
 	char filename[BUFFER_SIZE];
-	char serverHostname[BUFFER_SIZE];
+	char hostname[BUFFER_SIZE];
 
 	//init our strings
-	memset(buffer, 0, BUFFER_SIZE); //TODO this probably needs to be bigger
 	memset(filename, 0, BUFFER_SIZE);
-	memset(serverHostname, 0, BUFFER_SIZE);
+	memset(hostname, 0, BUFFER_SIZE);
 
 	//exe >> purpose >> port#
 	if (argc < 5){
@@ -41,15 +39,15 @@ int main(int argc, char *argv[]){
 		exit(0);
 	}
 
-	strcpy(serverHostname, argv[1]);
+	strcpy(hostname, argv[1]);
 	clientPort = atoi(argv[2]);
 	serverPort = atoi(argv[3]);
 	strcpy(filename, argv[4]);
 
 	//Check hostname
-	server = gethostbyname(argv[1]);//server name, return hostent
-	if (server == NULL){
-		fprintf(stderr,"Host does not exist\n");
+	host = gethostbyname(hostname);//server name, return hostent
+	if (host == NULL){
+		fprintf(stderr,"Hostname passed does not exist\n");
 		exit(0);
 	}
 
@@ -60,13 +58,31 @@ int main(int argc, char *argv[]){
 		exit(errno);
 	}
 
-	memset((char*) &serverAddr,0,sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;//For internet, IP address, address family
-	memmove((char*) &serverAddr.sin_addr.s_addr,(char*)server->h_addr,server->h_length);
-	serverAddr.sin_port = htons(serverPort);
+	//Setting up client sockaddr_in
+	memset((char*) &clientAddr,0,sizeof(clientAddr));
+	clientAddr.sin_family = AF_INET;//For internet, IP address, address family
+	memmove((char*) &clientAddr.sin_addr.s_addr,(char*)host->h_addr,host->h_length);
+	clientAddr.sin_port = htons(clientPort);
 
 	/*
-	//Check the connection
+	//Setting up server sockaddr_in //NOT NEEDED WITH sendMessage() FUNCTION!
+	memset((char *) &serverAddr,0,sizeof(serverAddr));
+	serverPort = atoi(argv[1]);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	serverAddr.sin_port = htons(serverPort);
+	*/
+
+	/*
+	//Check binding success //NOT NEEDED FOR UNCONNECTED UDP
+	if (bind(sockfd, (struct sockaddr *) &clientAddr,sizeof(clientAddr)) != 0) {//the socket, its cast, the size
+		fprintf(stderr,"Binding failed with error number %d\n", errno);
+		exit(errno);
+	}
+	*/
+
+	/*
+	//Check the connection //NOT NEEDED FOR UNCONNECTED UDP
 	int checkConnect = connect(sockfd,(struct sockaddr *)&server_address,sizeof(server_address));//(reference to socket by file descriptor,the specified address, address space of socket)
 	if (checkConnect < 0) {
 		fprintf(stderr,"Failed connection with error number: %d\n", errno);
@@ -74,25 +90,40 @@ int main(int argc, char *argv[]){
 	}
 	*/ //TODO remove this
 
-
 	char *joinRequest;
 	//Set up join request to send to server
 	asprintf(&joinRequest, "<join request>\n");
-	asprintf(&joinRequest, "%s%s %s\n", joinRequest, serverHostname, clientPort);
+	asprintf(&joinRequest, "%s%s %s\n", joinRequest, hostname, clientPort);
 	asprintf(&joinRequest, "%s%s\n", joinRequest, filename);
 	asprintf(&joinRequest, "%s</join request>\n");
 
 	//n = write(sockfd,joinRequest,strlen(joinRequest));//(reference to socket by file descriptor, the message written, write up to this length
-	n = sendto(sockfd,joinRequest,strlen(joinRequest),0,(struct sockaddr *)&serverAddr,sizeof(serverAddr));//for more info, see https://beej.us/guide/bgnet/html/multi/sockaddr_inman.html
-
+	//n = sendto(sockfd,joinRequest,strlen(joinRequest),0,(struct sockaddr *)&serverAddr,sizeof(serverAddr));//for more info, see https://beej.us/guide/bgnet/html/multi/sockaddr_inman.html
+	n = sendMessage(sockfd, hostname, serverPort, joinRequest); //MUCH shorter than above 2 lines!
 	//Check sendto success
 	if (n < 0){
 		fprintf(stderr,"sendto(server) failed with error number: %d\n", errno);
 		exit(errno);
 	}
 
+	//Wait for and receive token
+	struct sockaddr fromaddr;
+	memset(&fromaddr, 0, sizeof(fromaddr));
+	int bufferlen = recvfrom(sockfd, NULL, 0, MSG_PEEK, &fromaddr, sizeof(fromaddr)); //Gets length of message in socket buffer. MSG_PEEK specifies check socket buffer but leave it unread
+	if(bufferlen <0) {
+		fprintf(stderr, "Error receiving(peeking) message from server at the client, error number %d\n", errno);
+		exit(errno);
+	}
+	if(bufferlen<1) {
+		fprintf(stderr, "Client received empty message from server!\n");
+		exit(0);
+	}
+	char buffer[bufferlen+1]; //sets buffer size to exact length of message +1 char for null terminator
+	memset(buffer, 0, bufferlen+1); //init's the buffer
+	int messagelen = recvfrom(sockfd, buffer, bufferlen, 0, &fromaddr, sizeof(fromaddr)); //copies message into buffer
 
-	//TODO wait for token
+	fprintf(stdout, "Server response: \n%s\n", buffer);
+
 
 	//TODO remember to open the bbfile with mode a+ ( fopen("a.txt", "a+") )
 
